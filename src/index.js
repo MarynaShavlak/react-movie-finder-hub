590dacc29844d7d886edf1689a8a7d8c25b55c1b
Maryna Shavlak <shavlakmaryna@gmail.com>
2023-03-14 01:31:40 +0200

    basename

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ import './index.css';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <>
-      <BrowserRouter basename="/goit-react-hw-05-movies">
+      <BrowserRouter basename="/goit-react-hw-05-movies/">
         <ConfigProvider colors={getRandomColors()}>
           <App />
         </ConfigProvider>
